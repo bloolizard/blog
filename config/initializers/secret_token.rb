ff6cd1b141df703b7097324458b1eae2fe7bd5d9
Edwin Villanueva <edwincv0@gmail.com>
2013-12-13 11:56:39 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EdwinBlog::Application.config.secret_key_base = '87dd3ac0738830820271dd2edd73a2de17aab8330096ff4592585457e825f79b32f3b43ffda017bd00c782ce5722402cdd5bc9c0179e51550cca17eaa2ed3d20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EdwinBlog::Application.config.session_store :cookie_store, key: '_edwin_blog_session'
